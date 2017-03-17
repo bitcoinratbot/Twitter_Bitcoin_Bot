a6e8d122eaa330e327cd9e15b2543f554d886a2d
bitcoinratbot <blockchaindeveloper@outlook.com>
2017-03-17 17:06:15 +0000

    some_message

--- block.py
+++ block.py
@@ -12,16 +12,16 @@ myproxy = bitcoin.rpc.Proxy()
 block_info = myproxy.getblock(myproxy.getblockhash(myproxy.getblockcount()))
 
 #Setting these as variables will make them easier for future edits
-app_key =  ''
-app_secret = ''
-oauth_token =''
-oauth_token_secret = ''
+app_key =  'YHmHYHe9Yy13vE3ToRr9f3OVE'
+app_secret = 'QVo1ywyQVlMV1G1kV04LNIUJWaHbz4FMwNCV7i40yF2X4aY1tw'
+oauth_token ='832949363005652992-BeOBA9wBmjuHxqAIXgBweCbZmxuCCU8'
+oauth_token_secret = 'gZjspV2Ph5lEMNQbsJ9lgrt54GbCfb8nsCjldJUQvfGYq'
 
 #Prepare your twitter, you will need it for everything
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 #The above should just be a single line, without the break
 
-p2 = "No of Transactions: "
+p2 = "TXVol: "
 
 p3 = len(block_info.vtx)
 
@@ -29,21 +29,22 @@ U = p2 + str(p3)
 
 string = bitcoin.core.b2lx(block_info.GetHash());
 
-s = string.strip('0')
-
 b1 = "Block Hash: "
 
-O = b1 + s
+O = b1 + str(string)
 
 #lp = bitcoin.core.b2lx(block_info.GetHash())
 
 #O = b1 + str(lp)
 
-a = "Bitcoin CBlock Object Info: Height "
+a = "CBlock Object Info: Height "
 
 w = myproxy.getblockcount()
 
 P = a + str(w)
 
-twitter.update_status(status=P + "\n" + O + "\n" + U)
+
+s = twitter.update_status(status='New Block: ' + P + "\n" + O + "\n" + U)
+print (s)
+
 
